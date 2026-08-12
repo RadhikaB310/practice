3b2dd8c1d5ad56a9cf2bddd84b896833870a0041
RadhikaB310 <rr7898750@gamil.com>
2026-08-12 08:18:35 +0530

    login.java in main

---
+++ login.java
@@ -0,0 +1,7 @@
+public class login {
+    public static void main(String[] args) {
+        String username = "Radhika";
+        System.out.println("Welcome, " + username);
+        System.out.println("Login successful");
+    }
+}
